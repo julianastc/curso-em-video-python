510fa9f0dec3fab8fb35d89a032045ba083a60da
julianastc <julianasantostav@gmail.com>
2024-01-10 18:05:55 -0300

    Update first-commands.py

--- first-commands/first-commands.py
+++ first-commands/first-commands.py
@@ -1,6 +1,4 @@
 nome = input ("Qual é o seu nome?")
 idade = input ("Qual é a sua idade?")
 peso = input ("Quanto você pesa?")
-print ("Então, seu nome é", nome, ", você tem", idade, "anos e você pesa", peso , "kilos")
-
-
+print("Então, seu nome é "+ nome + " você tem "+ idade+ " anos e você pesa "+ peso+ " quilos")
