5e3683b093cf3930d2ed49031f48b834a19e17d5
julianastc <julianasantostav@gmail.com>
2024-01-10 18:13:38 -0300

    Create D2.py

---
+++ challenges/D2.py
@@ -0,0 +1,6 @@
+dia_nasc = input ("Que dia vc nasceu? ")
+mes_nasc = input ("Qual mês vc nasceu? ")
+ano_nasc = input ("Que ano vc nasceu? ")
+print ("Seu aniversário é em " + dia_nasc + "/" + mes_nasc + "/" + ano_nasc )
+
+
