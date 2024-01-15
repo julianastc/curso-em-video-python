4f217e5524fa9189cdf805cfa1267cb7fafce759
juliana <julianasantostav@gmail.com>
2024-01-15 17:33:25 -0300

    Update D3-s.py

--- challenges/D3-s.py
+++ challenges/D3-s.py
@@ -2,3 +2,4 @@ n1 = float(input('Digite um número: '))
 n2 = float(input('Digite mais um número: '))
 s = n1 + n2 
 print ('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
+#print(f'A soma entre {n1} e {n2} é igual a: {n1 + n2}')
