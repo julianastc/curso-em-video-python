2408ec948e8c3c4eac246c37f0c45ece4e0948cf
juliana <julianasantostav@gmail.com>
2024-01-15 17:17:29 -0300

    Update D3-s.py

--- challenges/D3-s.py
+++ challenges/D3-s.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite mais um número: '))
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite mais um número: '))
 s = n1 + n2 
 print ('A soma entre {0} e {1} vale {2}'.format(n1, n2, s))
