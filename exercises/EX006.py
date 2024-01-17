c79d54fdb3140bb2180ae69580b05b0cc4140bed
juliana <julianasantostav@gmail.com>
2024-01-17 20:22:22 -0300

    Create EX006.py

---
+++ exercises/EX006.py
@@ -0,0 +1,8 @@
+n = int(input("Digite um número: "))
+d = n*2
+t = n*3
+r = n**(1/2)
+print(f'''O dobro de {n} vale {d}.
+O triplo de {n} vale {t}.
+A raiz quadrada de {n} é igual {r:.2f}.
+    ''')
