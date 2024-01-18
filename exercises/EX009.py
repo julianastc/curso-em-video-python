bca1290c99dd773c6971d6fbc9d60466027170df
juliana <julianasantostav@gmail.com>
2024-01-17 21:57:00 -0300

    Create EX009.py

---
+++ exercises/EX009.py
@@ -0,0 +1,14 @@
+n = int(input("Digite um número para ver sua tabuada: "))
+print(f'''
+{n} x  1 = {n}
+{n} x  2 = {n*2}
+{n} x  3 = {n*3}
+{n} x  4 = {n*4}
+{n} x  5 = {n*5}
+{n} x  6 = {n*6}
+{n} x  7 = {n*7}
+{n} x  8 = {n*8}
+{n} x  9 = {n*9}
+{n} x 10 = {n*10}
+    ''')
+    
