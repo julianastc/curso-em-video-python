c5da8476516151abfda7ea8159e1a21b1735a7ec
juliana <julianasantostav@gmail.com>
2024-01-17 21:04:08 -0300

    Update EX007.py

--- exercises/EX007.py
+++ exercises/EX007.py
@@ -1,4 +1,4 @@
 n1 = int(input("Primeira nota do aluno: "))
 n2 = int(input("Segunda nota do aluno: "))
 m = (n1 + n2) / 2
-print (f"A média entre {n1} e {n2} é igual a {m}")
+print (f"A média entre {n1} e {n2} é igual a {m:.1f}")
