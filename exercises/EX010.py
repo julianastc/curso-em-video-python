bb6e16e03a92abc6a543ddd83ee834858be021c1
juliana <julianasantostav@gmail.com>
2024-01-19 16:17:49 -0300

    updatename

--- exercises/EX010.py
+++ exercises/EX010.py
@@ -1,4 +1,4 @@
 reais = float(input("Quanto dinheiro vc tem na carteira? R$"))
 dolar = 4.92
 valorConvertido = reais / dolar
-print(f"Com R${reais} vc pode comprar US${valorConvertido} ")
+print(f"Com R${reais:.2f} vc pode comprar US${valorConvertido:.2f} ")
