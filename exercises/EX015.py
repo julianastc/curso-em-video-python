c057eaf582be56391437d849ddf7315972c32923
juliana <julianasantostav@gmail.com>
2024-01-20 17:03:57 -0300

    uptint

--- exercises/EX015.py
+++ exercises/EX015.py
@@ -1,5 +1,5 @@
 # carro 60 por dia 0,15 por km 
-DaysUsed = float(input("Quantos dias foram usados? "))
+DaysUsed = int(input("Quantos dias foram usados? "))
 KmUsed = float(input("Quantos kilometros foram rodados? "))
 TotalValue = (DaysUsed * 60) + (KmUsed * 0.15)
-print(f"O total a pagar é {TotalValue}")
+print(f"O total a pagar é {TotalValue:.2f}")
