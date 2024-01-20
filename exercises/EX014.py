ef89024f72b54b7a2d67dbb36f9797a5781e759e
juliana <julianasantostav@gmail.com>
2024-01-20 16:57:57 -0300

    att

--- exercises/EX014.py
+++ exercises/EX014.py
@@ -1,3 +1,3 @@
 TempCelsius = float(input("Informe a temperatura em °C: "))
 TempFarenheint = TempCelsius * 1.8 + 32
-print(f"A temperatura de {TempCelsius:.1f}C° corresponde a {TempFarenheint:.2f}F°.") 
+print(f"A temperatura de {TempCelsius:.1f}C° corresponde a {TempFarenheint:.1f}F°.") 
