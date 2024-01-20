0c15b8517bdeffd3b35ae585e3e6ec9509dbfb45
juliana <julianasantostav@gmail.com>
2024-01-20 15:51:43 -0300

    Create EX013.py

---
+++ exercises/EX013.py
@@ -0,0 +1,3 @@
+OriginalSalary = float(input("Qual é o salário do funcionário? R$"))
+RaisedSalary = OriginalSalary + (OriginalSalary * (15/100))
+print(f"Um funcionário que ganhava {OriginalSalary:.2f}, com 15% de aumento, ganhará {RaisedSalary:.2f}")
