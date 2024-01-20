5fa9640f16076e0f8332bd4b83f5c1e42551a845
juliana <julianasantostav@gmail.com>
2024-01-20 15:51:39 -0300

    Create EX012.py

---
+++ exercises/EX012.py
@@ -0,0 +1,4 @@
+ProductOriginalPrice = float(input("Qual é o preço do produto? R$"))
+ProductDescontPrice = ProductOriginalPrice - (ProductOriginalPrice * (5/100)) 
+print(f"O produto que custava R${ProductOriginalPrice}, na promoção com desconto de 5% custará R${ProductDescontPrice:.2f}")
+#O produto que custava {ProductPrice}, na promoção com desconto de 5% vai custar xx
