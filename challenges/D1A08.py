fcc927cc8475e32203fe2483f37e014534e443b7
juliana <julianasantostav@gmail.com>
2024-01-27 18:39:03 -0300

    upd

--- challenges/D1A08.py
+++
@@ -1,3 +0,0 @@
-from math import floor
-numero = float(input("Digite um número: "))
-print(f"O número {numero} tem a parte inteira {floor(numero)}")
--- challenges/D2A08.py
+++
@@ -1,5 +0,0 @@
-from math import hypot
-cateto_oposto = int(input("Qual é o cateto oposto? "))
-cateto_adjascente = int(input("Qual é o cateto adjascente? "))
-hipotenusa = hypot(cateto_oposto, cateto_adjascente) 
-print(f"O valor da hipotenusa é {hipotenusa:.0f}.")
