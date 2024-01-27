5c2abcc5999e5fa774b519cb4013e1a5a7507ef9
Jessé Carvalho <60759322+jessecarvalho@users.noreply.github.com>
2024-01-27 19:24:44 -0300

    Update EX005.py

--- exercises/EX005.py
+++ exercises/EX005.py
@@ -1,4 +1,4 @@
-n = int(input("Digite um número: "))
-ant = n-1
-suc = n+1
-print(f"Analisando o valor {n}, seu antecessor é {ant} e seu sucessor é {suc}")
+numero = int(input("Digite um número: "))
+antecessor = n-1
+sucessor = n+1
+print(f"Analisando o valor {numero}, seu antecessor é {antecessor} e seu sucessor é {sucessor}")
