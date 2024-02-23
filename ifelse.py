fecf645e82bfdecfda77bd17ef80425548842114
juliana <julianasantostav@gmail.com>
2024-02-23 17:01:40 -0300

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,11 @@
+#For each digit between 0-9, the program will print a confirmation 
+#for the entered value or print "invalid inputs" for all other numbers.
+
+number = int(input("Digite um número: "))
+
+if (number == 0):
+    print("Entered 0")
+elif (number == 1):
+    print("Entered 1")
+else :
+    print("Invalid number")
