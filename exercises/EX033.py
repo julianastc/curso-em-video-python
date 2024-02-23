410aa3ca92de17ee4ce88f703d41e8064954ae27
juliana <julianasantostav@gmail.com>
2024-02-23 17:01:00 -0300

    Create EX033.py

---
+++ exercises/EX033.py
@@ -0,0 +1,11 @@
+payment = float(input('Qual é o seu salário atual? '))
+
+payment_base = 1251.00
+payment_increase_for_bigger_than_base = payment + (payment * (10/100))
+payment_increase_for_smaller_than_base = payment + (payment * (15/100))
+
+if payment >= payment_base:
+    print(f"O aumento será de 10%, ou seja {payment_increase_for_bigger_than_base:.2f}")
+else:
+    print(f'O aumento será de 15%, ou seja, {payment_increase_for_smaller_than_base:.2f}')
+
